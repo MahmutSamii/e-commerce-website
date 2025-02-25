0156e3090c6557b2273b70e9bcb881ea8105913e
MahmutSamii <samimahmut34@gmail.com>
2025-02-25 21:22:22 +0300

    Start the project

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <>
+            <div>
+                burası homepage
+            </div>
+        </>
+    );
+}
+
+export default Main;
